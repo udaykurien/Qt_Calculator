66076c38beea6cb11ae77974e4b068f122b2a63b
Uday <uday.kurien@gmail.com>
2026-04-15 02:28:14 -0400

    Basic functionality complete.

--- app.py
+++ app.py
@@ -45,6 +45,32 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('PyQt Calculator')
         self.resize(400, 600)
 
+        def click_me_parent(symbol):
+            def click_me():
+                self.compute_string = self.compute_string + str(symbol)
+                label.setText(self.compute_string)
+                print(self.compute_string)
+            return click_me
+
+        def click_equals():
+            try:
+                self.compute_string = str(eval(self.compute_string))
+                self.result = self.compute_string
+                label.setText(self.compute_string)
+            except:
+                label.setText("Error!")
+                self.compute_string = ""
+
+        def click_ce():
+            self.compute_string = ""
+            label.setText(self.compute_string)
+            print(self.compute_string)
+
+        def click_res():
+            self.compute_string = self.compute_string + str(self.result)
+            label.setText(self.compute_string)
+
+
         self.compute_string = ""
         label = QLabel("0")
         label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
@@ -58,12 +84,6 @@ class MainWindow(QMainWindow):
         # Row 0 - full width
         layout.addWidget(label, 0, 0, 1, 12)
 
-        def click_me_parent(symbol):
-            def click_me():
-                self.compute_string = self.compute_string + str(symbol)
-                label.setText(self.compute_string)
-                print(self.compute_string)
-            return click_me
 
         # Rows 2-4 - 3 equal parts of 12 (each spans 4)
         for num in range(1, 10):
@@ -71,7 +91,17 @@ class MainWindow(QMainWindow):
             btn.clicked.connect(click_me_parent(num))
             layout.addWidget(btn, ((num-1)//3)+3, ((num-1)%3)*4, 1, 4)
 
-        for num, symbol in enumerate([['+','-'],['*','/'], ['CE','=']]):
+        btn = ExpandingPushButton(str(0))
+        btn.clicked.connect(click_me_parent(0))
+        layout.addWidget(btn, 7, 0, 1, 4)
+        btn = ExpandingPushButton(str("."))
+        btn.clicked.connect(click_me_parent("."))
+        layout.addWidget(btn, 7, 4, 1, 4)
+        btn = ExpandingPushButton(str("Res"))
+        btn.clicked.connect(click_res)
+        layout.addWidget(btn, 7, 8, 1, 4)
+
+        for num, symbol in enumerate([['+','-'],['*','/']]):
             container = QWidget()
             container.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
             split = QVBoxLayout(container)
@@ -86,6 +116,18 @@ class MainWindow(QMainWindow):
             btn.clicked.connect(click_me_parent(symbol[1]))
 
             layout.addWidget(container, 2, num*4, 1, 4)
+
+        container = QWidget()
+        split = QVBoxLayout(container)
+        split.setContentsMargins(0,0,0,0)
+        btn = ExpandingPushButton(str("="))
+        split.addWidget(btn)
+        btn.clicked.connect(click_equals)
+        btn = ExpandingPushButton(str("CE"))
+        split.addWidget(btn)
+        btn.clicked.connect(click_ce)
+        layout.addWidget(container, 2, 8, 1, 4)
+
 
 
 
